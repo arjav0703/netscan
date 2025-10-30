de86aa5f226848b6c75ed2e2b44aa548e037b0b6
Arjav Jain <arjavjain0703@gmail.com>
2025-10-30 21:17:28 +0530

    enumerate possible IPs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 mod cli;
+use std::net::IpAddr;
+
 use anyhow::Result;
+use ipnetwork::IpNetwork;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = cli::load_config();
-    println!("Scanning network: {}", config.network);
+    let network: IpNetwork = config.network.parse()?;
+
+    println!("Scanning network: {network}");
+
+    println!(
+        "Possible IPs in the network: {}",
+        network.ips_in_network().len()
+    );
 
     Ok(())
+}
+
+trait IpNetworkExt {
+    fn ips_in_network(&self) -> Vec<IpAddr>;
+}
+
+impl IpNetworkExt for IpNetwork {
+    fn ips_in_network(&self) -> Vec<IpAddr> {
+        self.iter().collect()
+    }
 }
