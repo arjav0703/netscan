236a459e14cbe7eb8b539ef2d0f081c569ee2e48
Arjav Jain <arjavjain0703@gmail.com>
2025-10-30 21:50:56 +0530

    perform scan using ping

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod cli;
-use std::net::IpAddr;
-
 use anyhow::Result;
+use futures::future::join_all;
 use ipnetwork::IpNetwork;
+use std::net::IpAddr;
+use surge_ping::{Client, Config, PingIdentifier, PingSequence};
+use tokio::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -11,10 +13,18 @@ async fn main() -> Result<()> {
 
     println!("Scanning network: {network}");
 
-    println!(
-        "Possible IPs in the network: {}",
-        network.ips_in_network().len()
-    );
+    let ips = network.ips_in_network();
+    println!("Possible IPs in the network: {}", ips.len());
+
+    let tasks = ips.into_iter().map(|ip| {
+        tokio::spawn(async move {
+            if host_is_up(ip).await {
+                println!("{} responded to ping", ip);
+            }
+        })
+    });
+
+    let _ = join_all(tasks).await;
 
     Ok(())
 }
@@ -25,6 +35,22 @@ trait IpNetworkExt {
 
 impl IpNetworkExt for IpNetwork {
     fn ips_in_network(&self) -> Vec<IpAddr> {
-        self.iter().collect()
+        match self {
+            IpNetwork::V4(net) => net.iter().map(IpAddr::V4).collect(),
+            IpNetwork::V6(net) => net.iter().map(IpAddr::V6).collect(),
+        }
     }
+}
+
+async fn host_is_up(ip: IpAddr) -> bool {
+    let config = Config::default();
+    let client = match Client::new(&config) {
+        Ok(c) => c,
+        Err(_) => return false,
+    };
+
+    let mut pinger = client.pinger(ip, PingIdentifier(0)).await;
+    pinger.timeout(Duration::from_millis(800));
+
+    pinger.ping(PingSequence(0), &[]).await.is_ok()
 }
