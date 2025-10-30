11a2da03dc08275b9135acb5df4a7a8c5c109f11
Arjav Jain <arjavjain0703@gmail.com>
2025-10-30 22:14:09 +0530

    store hosts with a variable

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod cli;
 use anyhow::Result;
 use futures::future::join_all;
 use ipnetwork::IpNetwork;
-use std::net::IpAddr;
+use std::{net::IpAddr, sync::Arc};
 use surge_ping::{Client, Config, PingIdentifier, PingSequence};
 use tokio::time::Duration;
 
@@ -16,15 +16,26 @@ async fn main() -> Result<()> {
     let ips = network.ips_in_network();
     println!("Possible IPs in the network: {}", ips.len());
 
+    let up_hosts: Arc<tokio::sync::Mutex<Vec<IpAddr>>> =
+        Arc::new(tokio::sync::Mutex::new(Vec::new()));
+
     let tasks = ips.into_iter().map(|ip| {
+        let up_hosts = up_hosts.clone();
         tokio::spawn(async move {
             if host_is_up(ip).await {
-                println!("{} responded to ping", ip);
+                // println!("{} responded to ping", ip);
+                up_hosts.lock().await.push(ip);
             }
         })
     });
 
     let _ = join_all(tasks).await;
+
+    let up_hosts = up_hosts.lock().await;
+    println!("Hosts that are up:");
+    for ip in up_hosts.iter() {
+        println!("{}", ip);
+    }
 
     Ok(())
 }
